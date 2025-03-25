572750cda829c81f91e3f8c405b0e41736aa536c
keynguyen2004 <NguyenMinhVinhKy@gmail.com>
2025-03-24 21:03:39 -0500

    my_code.py: Add isdigit() check

--- my_code.py
+++ my_code.py
@@ -1,6 +1,14 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
+  # can only handle input with digits only
+  if not phone_num_to_fix.isdigit():
+    raise ValueError("Input must contain only digits")
+  
+  # can only handle numbers that are exactly 10 digits long
+  if (len(phone_num_to_fix) != 10):
+    raise ValueError("Can only format numbers that are exactly 10 digits long")
+  
   # given "5125558823". Split the parts, then recombine and return
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
