bcee2f293a2a6bb4d13d08a1fa328ec7e08079cc
keynguyen2004 <NguyenMinhVinhKy@gmail.com>
2025-03-24 20:56:18 -0500

    my_code.py: Test will fail

--- my_code.py
+++ my_code.py
@@ -12,3 +12,21 @@ def fix_phone_num(phone_num_to_fix):
 
 def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
+  assert fix_phone_num("5554429876") == '(555) 442 9876'
+  assert fix_phone_num("3216543333") == '(321) 654 3333'
+  
+def test_fix_phone_num_nonstandard_format():
+  
+  # Test will fail: Not 10 digits
+  with pytest.raises(ValueError):
+    fix_phone_num("51")
+  
+  # Test will fail: Non-digit 
+  with pytest.raises(ValueError):
+    fix_phone_num("555-442-98761")
+    
+  
+  # Test will fail: Non-digit 
+  with pytest.raises(ValueError):
+    fix_phone_num("(321) 654 3333") 
+  
